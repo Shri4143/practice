b8992e8da8e19e03e5a58259a158a3780fb2469d
Reshma s <reshmagowda011@gmail.com>
2022-10-08 10:29:53 +0530

    practice file

---
+++ devops.java
@@ -0,0 +1,9 @@
+devops is a methedology,linux and shellscripts are pre-requesits for devops.
+git is a distrubuted version control system.
+git hub is a hosted service for the git and its provides GUI
+
+SVN,TFS,CVS doesn't contains any local repository,
+	and it is a version control system.
+in git every developer contain one local repository.
+        and it is s distributed version control sysytem.
+
